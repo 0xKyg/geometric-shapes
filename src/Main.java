7b565f1f66c41a8c04d67483b84b98ee34ab338a
0xKyg <dalcal@protonmail.com>
2024-11-01 16:43:00 +0000

    Vertical direction added

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ public class Main {
         char symbol = '*';
         int nTimes = 10;
 
-        System.out.printf("%s\n", String.valueOf(symbol).repeat(nTimes));
-        for (int i = 1; i <= nTimes - 2; i++){
-            System.out.printf("%c%s%c\n", symbol," ".repeat(nTimes - 2), symbol);
-        }
-        System.out.printf("%s", String.valueOf(symbol).repeat(nTimes));
+        Rectangule rectangule = new Rectangule(symbol, nTimes, 'V');
+        rectangule.rectanguleRepresentation(rectangule);
+
     }
 }
